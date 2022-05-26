16c1b6998529bdb70afba99d43b31490ff6ca334
lizzy.liu <lavialiu@hotmail.com>
2022-05-26 11:19:18 +1000

    add seats

--- src/components/BookSeat.js
+++ src/components/BookSeat.js
@@ -5,9 +5,100 @@ import React from "react";
 function BookSeat() {
   //  const {movies, setMovies} = useContext(MoviesContext)
   return (
-    <div className="movie-choosen">
-      <h2>Booking confirmed</h2>
+    <>
+    
+      <div className="flex flex-col items-center">
+      <ul className="showcase flex" >
+      <li className="mx-5">
+        <div className="seat"></div>
+        <small>N/A</small>
+      </li>
+
+      <li className="mx-5">
+        <div className="seat selected"></div>
+        <small>Selected</small>
+      </li>
+
+      <li>
+        <div className="seat occupied"></div>
+        <small>Occupied</small>
+      </li>
+    </ul>
+
+    <div className="container">
+      <div className="screen"></div>
+      <div className="row">
+        <div className="seat"></div>
+        <div className="seat "></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+      </div>
+      <div className="row">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat occupied"></div>
+        <div className="seat occupied"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+      </div>
+
+      <div className="row">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat occupied"></div>
+        <div className="seat occupied"></div>
+      </div>
+
+      <div className="row">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+      </div>
+
+      <div className="row">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat occupied"></div>
+        <div className="seat occupied"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+      </div>
+
+      <div className="row">
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat"></div>
+        <div className="seat occupied"></div>
+        <div className="seat occupied"></div>
+        <div className="seat occupied"></div>
+        <div className="seat"></div>
+      </div>
     </div>
+
+    <p className="text">
+      You have selected <span id="count">0</span> seats for a price of $<span id="total">0</span>
+    </p>
+  
+      </div>
+    </>
   );
 }
 
